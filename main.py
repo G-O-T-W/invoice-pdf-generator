4d232a9f1296557f717132f3bd3da22df00fa0ac
G-O-T-W <64510900+G-O-T-W@users.noreply.github.com>
2024-10-20 01:34:47 +0530

    Create a invoice with just header in pdf format

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # Define the path to the directory containing the CSV files
 filepaths = glob.glob("invoices/*.xlsx")
 
+
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+    # Create a new PDF document
+    pdf = FPDF(orientation="p", unit="mm", format="A4")
+    pdf.add_page()
+
+    filename = Path(filepath).stem
+    invoice_nr = filename.split("-")[0]
+    date = filename.split("-")[1]
+
+    # Set invoice header
+    pdf.set_font("Times", size=18, style="B")
+    pdf.cell(w=0, h=9, txt=f"Invoice nr. {invoice_nr}", align="l", ln=1)
+    pdf.cell(w=0, h=9, txt=f"Date {date}", align="l", ln=1)
+
+    pdf.output(f"PDFs/{filename}.pdf")
+
+
+
+
 
 
 
